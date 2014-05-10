0a9a4f8c63e8b52518379aabaaa40998bb7d9408
Orhideous <orhideous@gmail.com>
2014-05-10 22:28:08 +0300

    Added Python 3.3 build

--- setup.py
+++ setup.py
@@ -1,6 +1,3 @@
-"""
-Based entirely on Django's own ``setup.py``.
-"""
 import os
 import sys
 from io import open
@@ -10,20 +7,11 @@ from distutils.command.install import INSTALL_SCHEMES
 try:
     from setuptools import setup
 except ImportError:
-    from distutils.core import setup  # NOQA
+    from distutils.core import setup
 
 
 class osx_install_data(install_data):
-    # On MacOS, the platform-specific lib dir is at:
-    #   /System/Library/Framework/Python/.../
-    # which is wrong. Python 2.5 supplied with MacOS 10.5 has an Apple-specific
-    # fix for this in distutils.command.install_data#306. It fixes install_lib
-    # but not install_data, which is why we roll our own install_data class.
-
     def finalize_options(self):
-        # By the time finalize_options is called, install.install_lib is set to
-        # the fixed directory, so we set the installdir to install_lib. The
-        # install_data class uses ('install_data', 'install_dir') instead.
         self.set_undefined_options('install', ('install_lib', 'install_dir'))
         install_data.finalize_options(self)
 
@@ -35,10 +23,7 @@ else:
 
 
 def fullsplit(path, result=None):
-    """
-    Split a pathname into components (the opposite of os.path.join) in a
-    platform-neutral way.
-    """
+    """ Split a pathname into components (the opposite of os.path.join) in a platform-neutral way."""
     if result is None:
         result = []
     head, tail = os.path.split(path)
@@ -48,14 +33,10 @@ def fullsplit(path, result=None):
         return result
     return fullsplit(head, [tail] + result)
 
-# Tell distutils to put the data_files in platform-specific installation
-# locations. See here for an explanation:
-# http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
+
 for scheme in INSTALL_SCHEMES.values():
     scheme['data'] = scheme['purelib']
 
-# Compile the list of packages available, because distutils doesn't have
-# an easy way to do this.
 packages, data_files = [], []
 root_dir = os.path.dirname(__file__)
 if root_dir != '':
@@ -63,7 +44,6 @@ if root_dir != '':
 enum_dir = 'django_enumfield'
 
 for dirpath, dirnames, filenames in os.walk(enum_dir):
-    # Ignore dirnames that start with '.'
     if os.path.basename(dirpath).startswith("."):
         continue
     if '__init__.py' in filenames:
@@ -76,25 +56,19 @@ version = __import__('django_enumfield').__version__
 setup(
     name="django-enumfield",
     version=version,
-
     description="Custom Django field for using enumerations of named constants",
-    long_description=open(
-        os.path.join(
-            os.path.dirname(__file__),
-            "README.rst"
-        ), encoding='utf-8'
-    ).read(),
-
+    long_description=open(os.path.join(os.path.dirname(__file__), "README.rst"), encoding='utf-8').read(),
     author="Hannes Ljungberg",
     author_email="hannes@5monkeys.se",
     url="http://github.com/5monkeys/django-enumfield",
-    download_url="https://github.com/5monkeys/django-enumfield/tarball/%s" % (version,),
+    download_url="https://github.com/5monkeys/django-enumfield/tarball/{version}".format(version=version),
     keywords=["django", "enum", "field", "status", "state", "choices", "form", "model"],
     platforms=['any'],
     license='MIT',
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         'Framework :: Django',
         "Natural Language :: English",
