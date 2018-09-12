311547990fb9e681458e90b330ce49e9c6eb1ef7
Akseli Nelander <anelander@gmail.com>
2018-09-12 13:12:19 +0200

    Add python 3.6 to classifiers

--- setup.py
+++ setup.py
@@ -73,6 +73,7 @@ setup(
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         'Framework :: Django',
         "Natural Language :: English",
         "Environment :: Web Environment",
