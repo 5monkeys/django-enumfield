ea3dd8d6d12bc2d767eb04b1792832561a874494
Akseli Nelander <anelander@gmail.com>
2019-07-11 00:15:19 +0200

    Print version info and make format on run_tests.py

--- run_tests.py
+++ run_tests.py
@@ -1,27 +1,35 @@
 #!/usr/bin/env python
+from __future__ import print_function
 
 import os
 import sys
+
 import django
 from django.conf import settings
 
 
 def delete_migrations():
     from os.path import exists, abspath, dirname, join
+
     migrations_dir = join(
-        dirname(abspath(__file__)), 'django_enumfield', 'tests', 'migrations')
+        dirname(abspath(__file__)), "django_enumfield", "tests", "migrations"
+    )
     if exists(migrations_dir):
-        os.system('rm -r ' + migrations_dir)
+        os.system("rm -r " + migrations_dir)
 
 
 def main():
-    import warnings
-    # Ignore deprecation warning caused by Django on 3.7 + 2.0
-    is_py37_django20 = (
-        sys.version_info[:2] == (3, 7) and django.VERSION[:2] == (2, 0)
+    print(
+        "Running tests for Python {} and Django {}".format(
+            ".".join(map(str, sys.version_info)), django.__version__
+        )
     )
+    import warnings
+
+    # Ignore deprecation warning caused by Django on 3.7 + 2.0
+    is_py37_django20 = sys.version_info[:2] == (3, 7) and django.VERSION[:2] == (2, 0)
     module = r"(?!django).*" if is_py37_django20 else ""
-    warnings.filterwarnings('error', module=module, category=DeprecationWarning)
+    warnings.filterwarnings("error", module=module, category=DeprecationWarning)
 
     delete_migrations()
 
@@ -30,60 +38,59 @@ def main():
         # get Django running tests
         settings.configure(
             INSTALLED_APPS=[
-                'django.contrib.auth',
-                'django.contrib.contenttypes',
-                'django.contrib.admin',
-                'django.contrib.sessions',
-                'django.contrib.messages',
-                'django_enumfield',
-                'django_enumfield.tests',
+                "django.contrib.auth",
+                "django.contrib.contenttypes",
+                "django.contrib.admin",
+                "django.contrib.sessions",
+                "django.contrib.messages",
+                "django_enumfield",
+                "django_enumfield.tests",
             ],
-            TEMPLATES=[{
-                'BACKEND': 'django.template.backends.django.DjangoTemplates',
-                'DIRS': [],
-                'APP_DIRS': True,
-                'OPTIONS': {
-                    'context_processors': [
-                        'django.contrib.auth.context_processors.auth',
-                        'django.contrib.messages.context_processors.messages',
-                    ]
+            TEMPLATES=[
+                {
+                    "BACKEND": "django.template.backends.django.DjangoTemplates",
+                    "DIRS": [],
+                    "APP_DIRS": True,
+                    "OPTIONS": {
+                        "context_processors": [
+                            "django.contrib.auth.context_processors.auth",
+                            "django.contrib.messages.context_processors.messages",
+                        ]
+                    },
                 }
-            }],
+            ],
             # Django replaces this, but it still wants it. *shrugs*
-            DATABASE_ENGINE='django.db.backends.sqlite3',
-            DATABASES={
-                'default': {
-                    'ENGINE': 'django.db.backends.sqlite3',
-                }
-            },
-            MEDIA_ROOT='/tmp/django_enums/',
-            MEDIA_PATH='/media/',
-            ROOT_URLCONF='django_enumfield.tests.urls',
+            DATABASE_ENGINE="django.db.backends.sqlite3",
+            DATABASES={"default": {"ENGINE": "django.db.backends.sqlite3"}},
+            MEDIA_ROOT="/tmp/django_enums/",
+            MEDIA_PATH="/media/",
+            ROOT_URLCONF="django_enumfield.tests.urls",
             DEBUG=True,
             TEMPLATE_DEBUG=True,
             MIDDLEWARE_CLASSES=[],
             MIDDLEWARE=[
-                'django.contrib.sessions.middleware.SessionMiddleware',
-                'django.contrib.messages.middleware.MessageMiddleware',
-                'django.contrib.auth.middleware.AuthenticationMiddleware'
+                "django.contrib.sessions.middleware.SessionMiddleware",
+                "django.contrib.messages.middleware.MessageMiddleware",
+                "django.contrib.auth.middleware.AuthenticationMiddleware",
             ],
         )
 
     # Compatibility with Django 1.7's stricter initialization
-    if hasattr(django, 'setup'):
+    if hasattr(django, "setup"):
         django.setup()
 
     from django.test.utils import get_runner
+
     test_runner = get_runner(settings)(verbosity=2, interactive=True)
-    if '--failfast' in sys.argv:
+    if "--failfast" in sys.argv:
         test_runner.failfast = True
 
-    failures = test_runner.run_tests(['django_enumfield'])
+    failures = test_runner.run_tests(["django_enumfield"])
 
     delete_migrations()
 
     sys.exit(failures)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
